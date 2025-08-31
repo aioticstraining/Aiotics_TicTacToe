23ed3043eb8aae46ddd7cd245e6107626f595adc
aioticstraining <aioticstraining@gmail.com>
2025-08-31 23:07:44 +0530

    Update tictactoe.cy.js

--- cypress/e2e/tictactoe.cy.js
+++ cypress/e2e/tictactoe.cy.js
@@ -1,20 +1,18 @@
 describe('TicTacToe Integration Test', () => {
+  it('should display the game page and allow a player to make a move', () => {
+    // Visit the application URL
+    cy.visit('http://localhost:9090');
 
-    it('should display the game page and allow a player to make a move', () => {
-        // Visit the URL where the Tic-Tac-Toe application is running
-        // We use localhost since the test container will be on the same network
-        cy.visit('http://localhost:9090');
+    // Wait for the game to load
+    cy.wait(2000);
 
-        // Check if the page contains the correct heading
-        cy.contains('h1', 'Tic Tac Toe').should('be.visible');
+    // Assert that the game board is visible
+    cy.get('#game-board').should('be.visible');
 
-        // Check if the game board is visible
-        cy.get('#game-board').should('be.visible');
+    // Make a move
+    cy.get('#cell-0').click();
 
-        // Click on the first square of the board
-        cy.get('[data-cell-index="0"]').click();
-
-        // Check if the square now contains an 'X'
-        cy.get('[data-cell-index="0"]').should('have.text', 'X');
-    });
+    // Assert that the cell now contains the "X" marker
+    cy.get('#cell-0').should('have.text', 'X');
+  });
 });
